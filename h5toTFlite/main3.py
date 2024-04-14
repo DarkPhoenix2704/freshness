ae55ee518b8ab88517d36110ea72bead10b6c052
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-04-14 21:00:42 +0530

    Revert "feat:some more features"
    This reverts commit b985afa8d856176800bef06cb2d8713b4096ac01.

--- h5toTFlite/main3.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-# Load the .pb file
-converter = tf.lite.TFLiteConverter.from_saved_model("model")
-tflite_model = converter.convert()
-with open("final_stale.tflite", "wb") as f:
-   f.write(tflite_model)
